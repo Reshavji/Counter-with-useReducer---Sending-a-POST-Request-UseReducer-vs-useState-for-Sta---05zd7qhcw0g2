541ce1ccd9311395390328df84db51abb6d8cbe0
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-06-06 15:22:41 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,14 @@
-const counterReducer = (state,action) =>{
+import React from 'react';
 
+const counterReducer = (state,action) =>{
+  console.log(state, action);
+  if(action.type === "INCREMENT"){
+    return state+1;
+  }
+  if(action.type === "DECREMENT"){
+    return state-1;
+  }
+  return state;
 }
 
 export {counterReducer}
